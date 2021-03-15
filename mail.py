d1c0b460300b2b59e2dce91c74c96e84f296fea9
DeluxeOwl <nospamplease0@protonmail.com>
2021-03-15 18:32:42 +0200

    Loaded mail configuration from .env

--- mail.py
+++ mail.py
@@ -11,18 +11,21 @@ from fastapi import (
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from pydantic import EmailStr, BaseModel
 from fastapi_mail.email_utils import DefaultChecker
-import mail_settings
+from dotenv import load_dotenv
 
+import os
+
+load_dotenv()
 
 conf = ConnectionConfig(
-    MAIL_USERNAME=mail_settings.MAIL_USERNAME,
-    MAIL_PASSWORD=mail_settings.MAIL_PASSWORD,
-    MAIL_FROM=mail_settings.MAIL_FROM,
-    MAIL_PORT=mail_settings.MAIL_PORT,
-    MAIL_SERVER=mail_settings.MAIL_SERVER,
-    MAIL_FROM_NAME=mail_settings.MAIL_FROM_NAME,
-    MAIL_TLS=True,
-    MAIL_SSL=False,
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT")),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
+    MAIL_TLS=True if os.getenv("MAIL_TLS") == "True" else False,
+    MAIL_SSL=True if os.getenv("MAIL_SSL") == "False" else False,
 )
 
 
