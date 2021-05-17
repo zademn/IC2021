f7b4103f68d09006f53ad04573356097dac63665
DeluxeOwl <nospamplease0@protonmail.com>
2021-05-17 16:52:53 +0300

    Added mail_port default as 587

--- mail.py
+++ mail.py
@@ -21,7 +21,7 @@ conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=int(os.getenv("MAIL_PORT")),
+    MAIL_PORT=int(os.getenv("MAIL_PORT") or 587),
     MAIL_SERVER=os.getenv("MAIL_SERVER"),
     MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
     MAIL_TLS=True if os.getenv("MAIL_TLS") == "True" else False,
